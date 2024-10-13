d8fb0e1af2e3eb6b572f27225d637dfcc7b08eb7
Denis Bochev <denisbo4ev@gmail.com>
2024-10-13 19:06:27 +0300

    home-work-10

--- src/js/1-timer.js
+++ src/js/1-timer.js
@@ -1,8 +1,3 @@
-import flatpickr from 'flatpickr';
-import 'flatpickr/dist/flatpickr.min.css';
-import iziToast from 'izitoast/dist/js/iziToast.min.js';
-import 'izitoast/dist/css/iziToast.min.css';
-
 let userSelectedDate = null;
 
 const startButton = document.querySelector('button[data-start]');
